dc8add3a8ddbc5615a359df7a36ccd53592ecfd7
parcioiu-vlad <parcioiuvlad@gmail.com>
2014-10-18 19:40:47 +0300

    version 1.1

---
+++ GitTest/src/RunnableTest.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vlad
+ * Date: 10/18/14
+ * Time: 6:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RunnableTest implements Runnable {
+    private ArrayList<Integer> list = new ArrayList<Integer>();
+    @Override
+    public void run() {
+        for(int i=0;i<100;i++) {
+            list.add(i);
+        }
+        System.out.println(Thread.currentThread().getName());
+    }
+
+    public ArrayList getList() {
+        return list;
+    }
+}
