4eba7f3f799b221a96180d390dcfd0db5deadcef
parcioiu-vlad <parcioiuvlad@gmail.com>
2014-10-18 12:20:44 +0300

    version 1.0

---
+++ GitTest/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: vlad
+ * Date: 10/18/14
+ * Time: 12:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main git test");
+    }
+}
